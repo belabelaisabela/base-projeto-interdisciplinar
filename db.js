4698f0ec93547317c6ad7825f9e7e8ec4592ea99
belabelaisabela <isabelagualberto7@gmail.com>
2023-05-29 17:22:29 -0400

    commit inicial proj

---
+++ db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+let connect = async function () {
+  try {
+    if (global.connection) {
+      return Promise.resolve(global.connection);
+    }
+
+    const pool = new Pool({
+      connectionString: 'postgres://postgres:postgres@localhost/picolo'
+    });
+
+    global.connection = pool;
+    return Promise.resolve(pool);
+  } catch (error) {
+    console.error('Erro ao estabelecer a conexão:', error);
+    throw error;
+  }
+};
+
+module.exports = { connect };
